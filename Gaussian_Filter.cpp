2c52ce16d327f29a49d7d549e0ac06f2b9fac3c3
LiliMeng <lilimeng1103@gmail.com>
2015-09-13 17:21:16 -0700

    Create Gaussian_Filter.cpp

---
+++ Gaussian_Filter.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+void filter(double gk[][5]); //declaration of function to create Gaussian filter
+
+int main()
+{
+		double gk[5][5];
+		filter(gk); //Function call to create a filter
+		for(int i=0; i<5; i++)  //loop to display the generated 5 x 5 Gaussian filter
+		{
+			for(int j=0; j<5; j++)
+			{
+				cout<<gk[i][j]<<"\t";
+			}
+			cout<<endl;
+		}
+	
+}
+
+void filter(double gk[][5])
+{
+		double stdv = 1.0;
+		double r,s=2.0*stdv*stdv;  // Assigning standard devitation to 1.0
+		double sum = 0.0; //Initialization of sun for normalization
+		for(int x=-2; x<=2; x++) //loop to generate 5x5 kernel
+		{
+			for(int y=-2; y<=2; y++)
+			{
+				r=sqrt(x*x+y*y);
+				gk[x+2][y+2]=(exp(-(r*r)/s))/(M_PI*s);
+				sum+=gk[x+2][y+2];
+			}
+		}
+		
+		for(int i=0; i<5; i++)  //loop to normalize the kernel
+		{
+			for(int j=0; j<5; j++)
+			{
+				gk[i][j]/=sum;
+			}
+		}
+}
